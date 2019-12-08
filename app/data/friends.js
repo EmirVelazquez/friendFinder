42a145d6f2855946c77054687d535ec28398e640
EmirVelazquez <emirleovelazquez@gmail.com>
2019-12-07 20:39:25 -0600

    added logic for posting new data into correct path /api/friends, data gets pushed into array as a json object

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,16 +9,16 @@ var friendArr = [
         name: "Sunny Obama",
         photo: "https://www.washingtonpost.com/resizer/tu1zinlpwciKPEQpCw1IlLADqwc=/1440x0/smart/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/EY3YKMD5UM4LDJI24Q2BU53HQ4.jpg",
         scores: [
-            5,
-            5,
-            3,
-            3,
-            5,
-            5,
-            5,
-            5,
-            1,
-            5
+            "5",
+            "5",
+            "3",
+            "3",
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5"
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,4 +14,8 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendData);
     });
+    app.post("/api/friends", function (req, res) {
+        friendData.push(req.body);
+        res.json(true);
+    });
 };
